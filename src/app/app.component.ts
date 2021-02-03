053cafb1094de53ece36fa21532e762485e26af1
Firoz <khan3firoz@gmail.com>
2021-02-04 01:22:42 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { templateJitUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'root-Component',
+    templateUrl:'./app.component.html'
 })
-export class AppComponent {
-  title = 'project1';
+export class AppComponent{
+    title: String;// it's model
+    products: any[];
+
+    constructor() {
+        this.title = 'Amozon retail shop';
+        this.getProducts();
+    }
+    getProducts() {
+        this.products = [
+            {name:'iphone', price:5000, category:'mobile'},
+            {name:'iphone12', price:54000, category:'mobile'},
+            {name:'iphone15', price:50050, category:'mobile'}
+        ]
+    }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from './components/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent,HeaderComponent,FooterComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap:[AppComponent]
 })
-export class AppModule { }
+
+export class AppModule{
+
+}
